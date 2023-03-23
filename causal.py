c8d1bbcdc191110165dedd68f2f6a33b15cdc14f
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2023-03-23 12:08:58 +0000

    sdf

--- causal.py
+++ causal.py
@@ -2,3 +2,7 @@ import streamlit as st
 import pandas as pd
 st.title('Automatic Causality')
 st.subheader('by Yulei')
+
+uploaded_file = st.file_uploader("Upload your dataset:")
+if uploaded_file is not None:
+    st.dataframe(uploaded_file)
