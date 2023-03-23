2c62be6f56c83ca2db70e4657526ce994a5a1e45
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2023-03-23 14:40:21 +0000

    Update causal.py

--- causal.py
+++ causal.py
@@ -25,5 +25,5 @@ if uploaded_file is not None:
             st.image('causal_model.png')
             estimands = model.identify_effect()
             st.write(estimands)
-            estimate = model.estimate_effect(estimands,method_name = "backdoor.iv.instrumental_variable")
+            estimate = model.estimate_effect(estimands,method_name = "backdoor.propensity_score_stratification")
             st.write(estimate)
