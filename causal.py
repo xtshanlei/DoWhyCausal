240e4aa6d9ecb77623abd3a8108785c25797a1ef
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2023-03-23 12:52:37 +0000

    Update causal.py

--- causal.py
+++ causal.py
@@ -14,7 +14,7 @@ if uploaded_file is not None:
     causal_graph= st.text_area('Please input the graph text')
     if causal_graph:
         treatment = st.selectbox("What's your treatment?",all_columns)
-        new_columns = treatment.remove(treatment)
+        new_columns = all_columns.remove(treatment)
         output = st.selectbox("What's your output?",new_columns)
         if treatment and output:
             from dowhy import CausalModel
